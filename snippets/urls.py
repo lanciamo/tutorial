95797006a8c985cd9c14e1d1f7f15a63c5195648
Leonid Kashin <lkashin@gmail.com>
2023-09-19 19:00:24 +0400

    init2

---
+++ snippets/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, re_path
+from snippets import views
+
+urlpatterns = [
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
+    re_path(r'^api/', views.schema_view),
+]
